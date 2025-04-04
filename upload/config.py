dd09d7233d059da271ca08f95253dec962976e4a
Tamim Dostyar <a.tamimdostyar@gmail.com>
2025-04-04 15:50:27 -0500

    config updated

--- upload/config.py
+++ upload/config.py
@@ -3,4 +3,4 @@
 try:
     backend_url = "http://localhost:8000"
 except:
-    backend_url = "https://team-shark.onrender.com"
+    backend_url = ["https://team-shark.onrender.com", "https://agforecast.netlify.app"]
