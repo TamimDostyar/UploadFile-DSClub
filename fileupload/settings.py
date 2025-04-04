9e5eff77d9a7e38195f0072d1896fdebeccfaaef
Tamim Dostyar <a.tamimdostyar@gmail.com>
2025-04-04 16:20:36 -0500

    added the items

--- fileupload/settings.py
+++ fileupload/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-sjw=851l3^g(xeojl#7)b2^iprb2l0i_kgxx65#jx*9y%o*1^z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'teamshark.onrender.com', '.onrender.com']
 
 # Custom user model
 AUTH_USER_MODEL = 'upload.Farmer'
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
